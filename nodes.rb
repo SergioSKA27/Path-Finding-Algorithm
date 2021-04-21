8ccd87887681d999358b8cd9add599d79e8cb91f
Sergio Lopez Martinez <70881259+SergioSKA27@users.noreply.github.com>
2021-04-20 23:18:31 -0500

    Add files via upload
    Modulo de la clase nodo añadido

---
+++ nodes.rb
@@ -0,0 +1,78 @@
+require 'ruby2d'
+
+set title: 'LABERINTOS'
+set background: 'white'
+set width: 1280
+set height: 720 
+set resizable: true
+
+=begin
+La clase nodo nos ayuda a dibujar las celdas ademas de que nos ayuda 
+a poder generar los laberintos de forma procedural.
+
+La clase se inicializa con la posicion(x,y) donde ubicaremos el centro de la celda 
+de la cual a partiran nuestras cuatro paredes. las cuales seran Objetos de la clase 
+Line de ruby2d.
+
+Adicionalmente podemos asignarle una etiqueta a cada nodo para identificarlo este 
+parametro es completamente opcional.
+
+=end
+class Node
+    attr_reader :x_pos , :y_pos, :color, :sqr, :size , :center
+    attr_reader :wall1, :wall2, :wall3, :wall4
+    @@father = nil
+    @@distance = 1e9
+    @@visited = 1
+    
+    def initialize(x, y, size, color ,*args )
+        @x_pos = x 
+        @y_pos = y
+        @color = color
+        @size = size
+        @wall1 = Line.new(
+            x1: x-10 , y1:y-10,
+            x2: x+20 , y2:y-10,
+            color: color, width: 5
+        )
+        @wall2 = Line.new(
+            x1: x+20 , y1:y-10,
+            x2: x+20 , y2:y+10,
+            color: color, width: 5
+        )
+
+        @wall3 = Line.new(
+            x1: x-10 , y1:y+10,
+            x2: x+20 , y2:y+10,
+            color: color, width: 5
+        )
+
+        @wall4 = Line.new(
+            x1: x-10 , y1:y-10,
+            x2: x-10 , y2:y+10,
+            color: color, width: 5
+        )
+        @center = Square.new(
+            x: x , y: y, 
+            color: 'blue' , size: size, 
+            z: 10
+        )
+    end
+
+    def show_node
+        @wall1
+        @wall3
+        @wall2
+        @center
+    end
+
+end
+#Basic matrix
+a = Node.new(30, 30, 1, 'black')
+b = Node.new(60, 30, 1, 'black')
+c = Node.new(30, 50, 1, 'black')
+d = Node.new(60, 50, 1, 'black')
+
+show
+
+
