882bc427f52fc66308b0dfa312bf83787d839379
SergioSka27 <sergioska81@hotmail.com>
2021-04-21 00:34:00 -0500

    grid añadido!

--- nodes.rb
+++ nodes.rb
@@ -30,34 +30,105 @@ class Node
         @y_pos = y
         @color = color
         @size = size
+        #Pared superior
         @wall1 = Line.new(
             x1: x-10 , y1:y-10,
             x2: x+20 , y2:y-10,
             color: color, width: 5
         )
+        #Pared derecha
         @wall2 = Line.new(
             x1: x+20 , y1:y-10,
             x2: x+20 , y2:y+10,
             color: color, width: 5
         )
-
+        #Pared inferior
         @wall3 = Line.new(
             x1: x-10 , y1:y+10,
             x2: x+20 , y2:y+10,
             color: color, width: 5
         )
-
+        #Pared izquierda
         @wall4 = Line.new(
             x1: x-10 , y1:y-10,
             x2: x-10 , y2:y+10,
             color: color, width: 5
         )
+        #centro(relativamete :v)
         @center = Square.new(
             x: x , y: y, 
             color: 'blue' , size: size, 
             z: 10
         )
     end
+
+
+    def wall1=(x)
+        @wall1 = Line.new(
+            x1: x-10 , y1:y-10,
+            x2: x+20 , y2:y-10,
+            color: 'white', width: 5
+        )
+    end
+
+    def wall2=(x)
+        @wall2 = Line.new(
+            x1: x+20 , y1:y-10,
+            x2: x+20 , y2:y+10,
+            color: 'white', width: 5
+        )
+    end
+
+    def wall3=(x)
+        @wall3 = Line.new(
+            x1: x-10 , y1:y+10,
+            x2: x+20 , y2:y+10,
+            color: 'white', width: 5
+        )
+    end
+
+    def wall4=(x)
+        @wall4 = Line.new(
+            x1: x-10 , y1:y-10,
+            x2: x-10 , y2:y+10,
+            color: 'white', width: 5
+        )
+    end
+
+
+
+
+    def deletewall(num)
+        if num == 1 then
+            @wall1 = Line.new(
+                x1: x-10 , y1:y-10,
+                x2: x+20 , y2:y-10,
+                color: 'white', width: 5
+            )
+        elsif num == 2 then
+            @wall2 = Line.new(
+                x1: x+20 , y1:y-10,
+                x2: x+20 , y2:y+10,
+                color: 'white', width: 5
+            )
+        elsif num == 3 then 
+            @wall3 = Line.new(
+                x1: x-10 , y1:y+10,
+                x2: x+20 , y2:y+10,
+                color: 'white', width: 5
+            )
+        else 
+            #Pared izquierda
+            @wall4 = Line.new(
+                x1: x-10 , y1:y-10,
+                x2: x-10 , y2:y+10,
+                color: 'white', width: 5
+            )
+        end
+
+    end
+
+    
 
     def show_node
         @wall1
@@ -67,12 +138,51 @@ class Node
     end
 
 end
+
+
+
+class Grid
+    attr_reader :rowsz, :colsz 
+    @@matrix = []
+    
+
+    def initialize(rows, colums)
+        x = 30
+        y = 30
+
+        @rowsz = rows
+        @colsz = colums
+        
+        for i in 0...rows do
+            r = []
+            xaux = x
+            for j in 0...colums do
+                a = Node.new(xaux,y,1,'black')
+                r.append(a);
+                xaux += 30
+            end
+            @@matrix.append(r)
+            y += 20
+        end
+    end
+end
+
 #Basic matrix
 a = Node.new(30, 30, 1, 'black')
-b = Node.new(60, 30, 1, 'black')
-c = Node.new(30, 50, 1, 'black')
-d = Node.new(60, 50, 1, 'black')
 
+tick = 0
+update do
+    if(tick == 10) then 
+        a.wall2(1)
+    end
+    tick += 1
+end
+#b = Node.new(60, 30, 1, 'black')
+#c = Node.new(30, 50, 1, 'black')
+#d = Node.new(60, 50, 1, 'black')
+
+
+#g = Grid.new(30,30)
 show
 
 
